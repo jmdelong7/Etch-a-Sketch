45451e01c19153a4ad5672fbba471bd7d8b1ee3c
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-04-21 21:23:40 -0700

    Create square grid
    Nested a for loop that creates a box in a for loop that creates container.
    The containers stack within the main container and stretch to box amount.

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const container = document.querySelector(".container");
 let squaresPerRow = 16;
 
 for (i = 0; i < squaresPerRow; i++) {
-  let square = document.createElement("div");
-  square.setAttribute("class", "grid-square");
-  container.appendChild(square);
+  let gridContainer = document.createElement("div");
+  gridContainer.setAttribute("class", "grid-container");
+  container.appendChild(gridContainer);
+  for (k = 0; k < squaresPerRow; k++) {
+    let square = document.createElement("div");
+    square.setAttribute("class", "grid-square");
+    gridContainer.appendChild(square);
+  }
 }
+
+// put squares into another container per row
