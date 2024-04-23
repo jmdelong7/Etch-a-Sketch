9903ee4d38901f1787e81745d0a994955dd96dc9
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-04-22 21:02:56 -0700

    Add user size choice for grid functionality
    Press button to get prompt and enter the size of grid you want!

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 const container = document.querySelector(".container");
 
-let squaresPerRow = 16;
-
-for (i = 0; i < squaresPerRow; i++) {
-  let gridContainer = document.createElement("div");
-  gridContainer.setAttribute("class", "grid-container");
-  container.appendChild(gridContainer);
-  
-  for (k = 0; k < squaresPerRow; k++) {
-    let square = document.createElement("div");
-    square.setAttribute("class", "grid-square");
-    gridContainer.appendChild(square);
-  }
-}
-
 function changeColor(target, color) {
   target.style.backgroundColor = color;
 }
 
-let squares = document.querySelectorAll("div.grid-square");
-squares.forEach(sq => {
-  sq.addEventListener("mouseover", function() {changeColor(sq, "blue")})
+function makeGrid(squaresPerRow) {
+  for (i = 0; i < squaresPerRow; i++) {
+    let gridContainer = document.createElement("div");
+    gridContainer.setAttribute("class", "grid-container");
+    container.appendChild(gridContainer);
+    
+    for (k = 0; k < squaresPerRow; k++) {
+      let square = document.createElement("div");
+      square.setAttribute("class", "grid-square");
+      gridContainer.appendChild(square);
+    }
+  }  
+}
+
+function inputPopup() {
+  let numSquares = prompt("Enter number of squares per side.");
+  makeGrid(numSquares);
+  
+  let squares = document.querySelectorAll("div.grid-square");
+  squares.forEach(sq => {
+    sq.addEventListener("mouseover", function() {changeColor(sq, "blue")})
 })
+
+}
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", inputPopup);
+
+
+
+
